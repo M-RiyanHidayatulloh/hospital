01b1e08330ee6f057033f4100ffe337a525e0054
RiyanHidayatulloh <Riyanska05@gmail.com>
2024-06-12 00:38:45 +0700

    hhh

--- routes/web.php
+++ routes/web.php
@@ -36,8 +36,8 @@ Route::get('/', [HomeController::class, 'home']);
 
 Route::middleware(['auth', 'verified', 'user'])->group(function () {
     Route::get('users/dashboard', [HomeController::class, 'user'])->name('dashboard');
-    });
-    
+});
+
 
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -48,9 +48,9 @@ Route::group(['middleware' => ['auth']], function () {
 
 
 
-//     Route::group(['middleware' => ['auth', 'doctor']], function () {
+    //     Route::group(['middleware' => ['auth', 'doctor']], function () {
 //         Route::resource('my_schedule', DoctorScheduleController::class);
-  
+
     // Route::group(['middleware' => ['auth', 'doctor']], function () {
     //     Route::resource('my_schedule', DoctorScheduleController::class);
     // });
@@ -63,7 +63,7 @@ Route::group(['middleware' => ['auth']], function () {
         Route::get('/admin/schedules/edit/{id}', [DoctorScheduleController::class, 'edit'])->name('admin/schedules/edit');
         Route::put('/admin/schedules/edit/{id}', [DoctorScheduleController::class, 'update'])->name('admin/schedules/update');
         Route::get('admin/schedules/delete/{id}', [DoctorScheduleController::class, 'delete'])->name('admin/schedules/delete');
-      
+
         Route::get('/admin/doctors', [DoctorController::class, 'index'])->name('admin/doctors');
         Route::get('/admin/doctors/create', [DoctorController::class, 'create'])->name('admin/doctors/create');
         Route::post('/admin/doctors/store', [DoctorController::class, 'store'])->name('admin/doctors/store');
@@ -73,66 +73,66 @@ Route::group(['middleware' => ['auth']], function () {
         Route::get('admin/doctors/trash', [DoctorController::class, 'trash'])->name('admin/doctors/trash');
         Route::get('admin/doctors/restore/{id?}', [DoctorController::class, 'restore'])->name('admin/doctors/restore');
         Route::get('admin/doctors/destroy/{id?}', [DoctorController::class, 'destroy'])->name('admin/doctors/destroy');
-      
+
         Route::get('/admin/appointments', [AppointmentController::class, 'index'])->name('admin/appointments');
         Route::get('/admin/appointments/create', [AppointmentController::class, 'create'])->name('admin/appointments/create');
         Route::post('/admin/appointments/store', [AppointmentController::class, 'store'])->name('admin/appointments/store');
         Route::get('admin/appointments/edit/{id}', [AppointmentController::class, 'edit'])->name('admin/appointments/edit');
         Route::put('/admin/appointments/edit/{id}', [AppointmentController::class, 'update'])->name('admin/appointments/update');
         Route::get('admin/appointments/delete/{id}', [AppointmentController::class, 'delete'])->name('admin/appointments/delete');
-      
+
         Route::get('/admin/patients', [PatientController::class, 'index'])->name('admin/patients');
         Route::get('/admin/patients/create', [PatientController::class, 'create'])->name('admin/patients/create');
         Route::post('/admin/patients/store', [PatientController::class, 'store'])->name('admin/patients/store');
         Route::get('admin/patients/edit/{id}', [PatientController::class, 'edit'])->name('admin/patients/edit');
         Route::put('/admin/patients/edit/{id}', [PatientController::class, 'update'])->name('admin/patients/update');
         Route::get('admin/patients/delete/{id}', [PatientController::class, 'delete'])->name('admin/patients/delete');
-      
+
         Route::get('/admin/queues', [QueueController::class, 'index'])->name('admin/queues');
         Route::get('/admin/queues/create', [QueueController::class, 'create'])->name('admin/queues/create');
         Route::post('/admin/queues/store', [QueueController::class, 'store'])->name('admin/queues/store');
         Route::get('/admin/queues/edit/{id}', [QueueController::class, 'edit'])->name('admin/queues/edit');
         Route::put('/admin/queues/edit/{id}', [QueueController::class, 'update'])->name('admin/queues/update');
         Route::get('/admin/queues/delete/{id}', [QueueController::class, 'delete'])->name('admin/queues/delete');
-      
+
         Route::get('/admin/rooms', [RoomController::class, 'index'])->name('admin/rooms');
         Route::get('/admin/rooms/create', [RoomController::class, 'create'])->name('admin/rooms/create');
         Route::post('/admin/rooms/store', [RoomController::class, 'store'])->name('admin/rooms/store');
         Route::get('/admin/rooms/edit/{id}', [RoomController::class, 'edit'])->name('admin/rooms/edit');
         Route::put('/admin/rooms/edit/{id}', [RoomController::class, 'update'])->name('admin/rooms/update');
         Route::get('/admin/rooms/delete/{id}', [RoomController::class, 'delete'])->name('admin/rooms/delete');
-        
+
         Route::get('/admin/online_consultations', [OnlineConsultationController::class, 'index'])->name('admin/online_consultations');
         Route::get('/admin/online_consultations/create', [OnlineConsultationController::class, 'create'])->name('admin/online_consultations/create');
         Route::post('/admin/online_consultations/store', [OnlineConsultationController::class, 'store'])->name('admin/online_consultations/store');
         Route::get('/admin/online_consultations/edit/{id}', [OnlineConsultationController::class, 'edit'])->name('admin/online_consultations/edit');
         Route::put('/admin/online_consultations/edit/{id}', [OnlineConsultationController::class, 'update'])->name('admin/online_consultations/update');
         Route::get('/admin/online_consultations/delete/{id}', [OnlineConsultationController::class, 'delete'])->name('admin/online_consultations/delete');
-      
+
         Route::get('/admin/medical_records', [MedicalRecordController::class, 'index'])->name('admin/medical_records');
         Route::get('/admin/medical_records/create', [MedicalRecordController::class, 'create'])->name('admin/medical_records/create');
         Route::post('/admin/medical_records/store', [MedicalRecordController::class, 'store'])->name('admin/medical_records/store');
         Route::get('/admin/medical_records/edit/{id}', [MedicalRecordController::class, 'edit'])->name('admin/medical_records/edit');
         Route::put('/admin/medical_records/edit/{id}', [MedicalRecordController::class, 'update'])->name('admin/medical_records/update');
         Route::get('/admin/medical_records/delete/{id}', [MedicalRecordController::class, 'delete'])->name('admin/medical_records/delete');
-      
+
         Route::get('/admin/payments', [PaymentController::class, 'index'])->name('admin/payments');
         Route::get('/admin/payments/create', [PaymentController::class, 'create'])->name('admin/payments/create');
         Route::post('/admin/payments/store', [PaymentController::class, 'store'])->name('admin/payments/store');
         Route::get('/admin/payments/edit/{id}', [PaymentController::class, 'edit'])->name('admin/payments/edit');
         Route::put('/admin/payments/edit/{id}', [PaymentController::class, 'update'])->name('admin/payments/update');
         Route::get('/admin/payments/delete/{id}', [PaymentController::class, 'delete'])->name('admin/payments/delete');
-      
+
         Route::get('/admin/health_informations', [HealthInformationController::class, 'index'])->name('admin/health_informations');
         Route::get('/admin/health_informations/create', [HealthInformationController::class, 'create'])->name('admin/health_informations/create');
         Route::post('/admin/health_informations/store', [HealthInformationController::class, 'store'])->name('admin/health_informations/store');
         Route::get('/admin/health_informations/edit/{id}', [HealthInformationController::class, 'edit'])->name('admin/health_informations/edit');
         Route::put('/admin/health_informations/edit/{id}', [HealthInformationController::class, 'update'])->name('admin/health_informations/update');
         Route::get('/admin/health_informations/delete/{id}', [HealthInformationController::class, 'delete'])->name('admin/health_informations/delete');
->
+
     });
 
-        Route::middleware(['auth'])->group(function () {
+    Route::middleware(['auth'])->group(function () {
         Route::resource('users', RegisteredUserController::class);
         Route::resource('patients', PatientController::class);
         Route::resource('doctors', DoctorController::class);
@@ -146,7 +146,7 @@ Route::group(['middleware' => ['auth']], function () {
         Route::resource('doctor_schedules', DoctorScheduleController::class);
         Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     });
-    
+
 
 
 
